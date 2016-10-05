56931b4650627ce0d462c8186a5b27ce7d1c4d98
Juliane Blier <blier.juliane@gmail.com>
2016-10-05 14:26:18 +0200

    Finalisation de la step 3, jeu fonctionnel

--- nombreMystere/index-step3.js
+++ nombreMystere/index-step3.js
@@ -1,12 +1,27 @@
 $(document).ready(main);
 
-// Fonction principale
+
 function main(){
 
+	function aleatoire(min, max){
+		random = Math.floor((max - min + 1) * Math.random()) + min;
+		console.log('Nombre random = ' + random);
+		return random;
+	}
+
 	function demarrerPartie(){
-		tryLeft = 15;
-		$('span').html(tryLeft);
-		nombreMystere = Math.floor(Math.random() * 101);
+		tryLeft = prompt('Choisissez le nombre maximal de tentatives');
+		$('#tryAgain').html(tryLeft);
+
+		min = parseInt(prompt('Choisissez le nombre minimum du Nombre Mystère'));
+		$('#min').html(min);
+		//console.log(typeof(min));
+		max = parseInt(prompt('Choisissez le nombre maximum du nombreMystere'));
+		$('#max').html(max);
+		//console.log(typeof(max));
+
+		nombreMystere = aleatoire(min, max);
+		//nombreMystere = Math.floor(Math.random() * 101);
 		console.log('Nombre Mystère = ' + nombreMystere);
 	}
 
@@ -16,74 +31,34 @@ function main(){
 	}
 	
 	function partiePerdue(){
-		alert('Dommage, tu as épuisé tes 15 tentatives. Le nombre mystère était ' + nombreMystere +'.');
+		alert('Dommage, tu as épuisé toutes tes tentatives. Le nombre mystère était ' + nombreMystere +'.');
 		demarrerPartie();
 	}
 
-	// Créer et initialiser une variable 'globale' qui 
-	// va stocker le nombre de tentatives restantes.
-	// Créer et initialiser une variable 'globale' qui 
-	// va stocker le nombre "aléatoire" mystère.
-	demarrerPartie();
 
-
-	// == Fonction clickValider == 
-	// Créer la fonction 'clickValider'
 	function clickValider(){
-
-
-		// Récupérer le contenu de mon input
 		var tryNow = parseInt($('input').val());
-		// Comparer ce contenu au nombre mystère.
-			/*
-				Si le contenu est égal : 
-				- afficher une alert Gagné
-				- démarrer une nouvelle partie
-			*/
-			if (tryNow === nombreMystere) {
-				partieGagnee();
-			}
-			// Sinon s'il est supérieur
-			// Afficher une alert Perdu, votre nombre est trop grand
-			else if (tryNow > nombreMystere) {
-				alert('Perdu, votre nombre est trop grand.');
-				tryLeft--; 
-			}
-			// Sinon s'il est inférieur
-			// Afficher une alert Perdu, votre nombre est trop petit
-			else if(tryNow < nombreMystere) {
-				alert('Perdu, votre nombre est trop petit.');
-				tryLeft--;
-			}
-			// Dans les cas où c'est perdu, diminuer le nombre de tentatives
+		if (tryNow === nombreMystere) {
+			partieGagnee();
+		}
+		else if (tryNow > nombreMystere) {
+			alert('Perdu, votre nombre est trop grand.');
+			tryLeft--; 
+		}
+		else if(tryNow < nombreMystere) {
+			alert('Perdu, votre nombre est trop petit.');
+			tryLeft--;
+		}
 
-			// Actualiser la zone html affichant le nombre de tentatives
-			$('span').html(tryLeft);
-			/* 
-				Si le nb de tentatives est égal à 0,
-			 	la partie est perdue :
-			 	- afficher que c'est perdu
-			 	- démarrer une nouvelle partie
-			 		+ remettre le nombre de tentatives à sa valeur initiale
-			 		+ générer un nouveau nombre mystère
-			 */
-			 if(tryLeft === 0){
-			 	partiePerdue();
-			 }
-			
-	// /Fin == Fonction clickValider == 
+		$('#tryAgain').html(tryLeft);
+	
+		 if(tryLeft === 0){
+		 	partiePerdue();
+		}
 	}
 
-	// Ecrire le code qui va détecter que le bouton 'valider' est cliqué.
-	// Et qui appelle la fonction 'clickValider' défini ci-dessus
+	demarrerPartie();
 	$('button').click(function(){
 		clickValider();
-	})
-
-	/*
-		Concernant la 'refactorisation' : 
-		- créer une fonction 'demarrerPartie'
-		- créer une fonction 'partieGagnee'
-		- créer une fonction 'partiePerdue'
-	*/
+	});
 }
